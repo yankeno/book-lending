250104f7b63a109ca8d1b3342d6e8bee69ddbe3a
yankeno <kn5sttyryh@gmail.com>
2023-01-02 20:54:49 +0900

    [fix]アプリケーションロゴの変更
    ・ロゴ変更
    ・favicon 追加

--- src/resources/views/auth/confirm-password.blade.php
+++ src/resources/views/auth/confirm-password.blade.php
@@ -1,9 +1,11 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <div class="w-12">
+                <a href="/">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </div>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
@@ -20,10 +22,8 @@
             <div>
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="flex justify-end mt-4">
